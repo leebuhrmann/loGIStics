3fa3ed7fb79d0fcafd2fcff7af7a50c40855097a
regannd <rndelp@gmail.com>
2024-02-05 20:52:48 -0600

    issue-9 initilize gradle project with spring boot

---
+++ snow-api/src/main/java/com/logistics/snowapi/SnowApiApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.snowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowApiApplication.class, args);
+	}
+
+}
