eea3be26d1b6166a56288b95f61e6215b149a2cc
Arber <asalihu@unomaha.edu>
2024-02-12 02:48:34 -0600

    This was supposed to be a test branch

---
+++ snow/src/components/fetchData.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import {fetchData} from '../services/ApiServices';
+
+function MyComponent(){
+    const [data, setData] = useState<string | null>(null);
+
+    useEffect(()=>{
+        fetchData()
+        .then(response => {
+            setData(response);
+        })
+        .catch(console.error);
+    }, []);
+
+    return(
+        <div>
+          {data ? <p>{data}</p> : <p>Loading...</p>}
+        </div>
+    );
+}
+
+export default MyComponent;
