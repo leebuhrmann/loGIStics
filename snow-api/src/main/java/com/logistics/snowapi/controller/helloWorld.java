de6caba9a5e71132a5dbbf49e389abe4b90c16dc
Arber <asalihu@unomaha.edu>
2024-02-13 19:13:32 -0600

    formatting backend

--- snow-api/src/main/java/com/logistics/snowapi/controller/helloWorld.java
+++ snow-api/src/main/java/com/logistics/snowapi/controller/helloWorld.java
@@ -3,11 +3,12 @@ package com.logistics.snowapi.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class helloWorld {
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello() {
         return "Hello world from Backend";
     }
 }
