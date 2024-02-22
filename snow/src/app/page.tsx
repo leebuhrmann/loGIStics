57fc4cc8029500550d07efca62b310be78fbb94c
kmedlin18m <kirsten18m@gmail.com>
2024-02-21 19:02:58 -0600

    Issue-20 updates with Regan's solution and changed map padding

--- snow/src/app/page.tsx
+++ snow/src/app/page.tsx
@@ -16,32 +16,40 @@ import {
 
 export default function Home() {
   return (
-    <main className={"w-screen h-screen overflow-hidden"}>
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    User Account
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
-        <ResizablePanelGroup
-            direction="horizontal"
-            className="w-screen min-h-screen rounded-lg border"
-        >
-            <ResizablePanel defaultSize={15}>
-                <div className="flex h-full items-center justify-center p-6">
-                    <Button>Button</Button>
-                </div>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel defaultSize={75}>
-                <div className="flex h-full items-center justify-center p-6">
-                    <Hello/>
-                    <MapComponent/>
-                </div>
-            </ResizablePanel>
-            <ResizableHandle />
-        </ResizablePanelGroup>
-    </main>
-  );
+      <main className="w-screen h-screen overflow-hidden">
+          <div className="h-5/100">
+              <NavigationMenu>
+                  <NavigationMenuList>
+                      <NavigationMenuItem>
+                          User Account
+                      </NavigationMenuItem>
+                  </NavigationMenuList>
+              </NavigationMenu>
+              <ResizablePanelGroup
+                  direction="horizontal"
+                  className="w-screen min-h-screen rounded-lg border"
+              >
+                  <ResizablePanel defaultSize={20}>
+                      <div className="flex h-full p-6 overflow-auto">
+                          {/* <Button>Button</Button> */}
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                              sunt in culpa qui officia deserunt mollit anim id est laborum -- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
+                      </div>
+                  </ResizablePanel>
+                  <ResizableHandle/>
+                  <ResizablePanel defaultSize={80}>
+                      <div className="flex h-full items-center justify-center">
+                          <Hello/>
+                          <MapComponent/>
+                      </div>
+                  </ResizablePanel>
+                  <ResizableHandle/>
+              </ResizablePanelGroup>
+          </div>
+      </main>
+);
 }
