a1b827bd94f52a22235986d487acf391d793ffe1
kmedlin18m <kirsten18m@gmail.com>
2024-02-22 18:22:35 -0600

    Issue-20 formmatting and no scroll

--- snow/src/app/page.tsx
+++ snow/src/app/page.tsx
@@ -1,55 +1,84 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import MapComponent from "../components/map/MapComponent";
 import Hello from "../components/map/fetchData";
 import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuList,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
 } from "@/components/ui/navigation-menu";
-
 
 export default function Home() {
   return (
-      <main className="w-screen h-screen overflow-hidden">
-          <div className="h-5/100">
-              <NavigationMenu>
-                  <NavigationMenuList>
-                      <NavigationMenuItem>
-                          User Account
-                      </NavigationMenuItem>
-                  </NavigationMenuList>
-              </NavigationMenu>
-              <ResizablePanelGroup
-                  direction="horizontal"
-                  className="w-screen min-h-screen rounded-lg border"
-              >
-                  <ResizablePanel defaultSize={20}>
-                      <div className="flex h-full p-6 overflow-auto">
-                          {/* <Button>Button</Button> */}
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                              sunt in culpa qui officia deserunt mollit anim id est laborum -- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
-                      </div>
-                  </ResizablePanel>
-                  <ResizableHandle/>
-                  <ResizablePanel defaultSize={80}>
-                      <div className="flex h-full items-center justify-center">
-                          <Hello/>
-                          <MapComponent/>
-                      </div>
-                  </ResizablePanel>
-                  <ResizableHandle/>
-              </ResizablePanelGroup>
+    <main className="w-screen h-screen overflow-hidden">
+      <div className="h-5/100">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>User Account</NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="w-screen min-h-screen rounded-lg border"
+      >
+        <ResizablePanel defaultSize={20}>
+          <div className="flex h-full p-6 overflow-hidden">
+            {/* <Button>Button</Button> */}
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum --
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore euLorem ipsum
+              dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum -- Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore euLorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt mollit anim id est laborum -- Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu
+            </p>
           </div>
-      </main>
-);
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={80}>
+          <div className="flex h-full items-center justify-center">
+            <Hello />
+            <MapComponent />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+      </ResizablePanelGroup>
+    </main>
+  );
 }
