c32a13db9970be99085fd0bf3aea0e4650739727
lee <lee.buhrmann@gmail.com>
2024-03-20 11:41:22 -0500

    Added feature id as a collected json property.

--- snow-api/src/main/java/com/logistics/snowapi/geojsonresponse/Feature.java
+++ snow-api/src/main/java/com/logistics/snowapi/geojsonresponse/Feature.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Feature {
+    @JsonProperty("id")
+    private String id;
+
     @JsonProperty("properties")
     private FeatureProperties properties;
 
@@ -16,13 +19,22 @@ public class Feature {
         this.properties = properties;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String toString() {
-        return String.format("event: %s" +
+        return String.format("id: %s" +
+                            "\nevent: %s" +
                             "\nheadline: %s" +
                             "\nonset: %s" +
                             "\nexpires: %s" +
                             "\nUGC: %s" +
                             "\ndescription: %s",
-                properties.getEvent(), properties.getHeadline(), properties.getOnset(), properties.getExpires(), properties.getUGC(), properties.getDescription());
+                this.id, properties.getEvent(), properties.getHeadline(), properties.getOnset(), properties.getExpires(), properties.getUGC(), properties.getDescription());
     }
 }
